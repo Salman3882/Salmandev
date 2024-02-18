add0dd0aed7a2bba5e1f7edab586aabcda7f8dc4
Salman3882 <selmnankhan.627@gmail.com>
2024-02-18 17:07:32 +0000

    first commit

---
+++ js/index.js
@@ -0,0 +1,56 @@
+
+// /=== Animations ===//
+
+AOS.init(
+    {
+        offset: 100,
+        duration: 1000,
+        easing: 'ease-in-out', // default easing for AOS animations
+        once: true,
+        mirror:false,
+        anchorPlacement: 'top-bottom'
+    }
+);
+
+
+// =====Toggle icon navbar====//
+
+let menuIcon = document.querySelector('#menu-icon')
+let navbar = document.querySelector('.navbar')
+
+menuIcon.onclick = ()=>{
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+    
+    navLinks.forEach(navLink => {
+        navLink.addEventListener('click', () => {
+            navbar.classList.remove('active'); 
+            menuIcon.classList.remove('bx-x')
+            
+        });
+    });
+}
+// =====Scroll section active look====//
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll= ()=>{
+    sections.forEach(sec=>{
+        let top = window.scrollY;
+        let offset =  sec.offsetTop - 150;
+        let height = sec.offsetHeight;;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(links=>{
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+                
+            })
+        }
+    })
+    // =====sticky navbar====//
+    let header = document.querySelector('header')
+    header.classList.toggle('sticky', window.scrollY>100)
+}
