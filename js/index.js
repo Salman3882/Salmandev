43eb26b9b86ed3c4f88c4e64b131cebe61cecefd
Salman3882 <selmnankhan.627@gmail.com>
2024-02-24 06:14:24 +0000

    email is working

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ AOS.init(
         duration: 1000,
         easing: 'ease-in-out', // default easing for AOS animations
         once: true,
-        mirror:false,
+        mirror: false,
         anchorPlacement: 'top-bottom'
     }
 );
@@ -18,15 +18,15 @@ AOS.init(
 let menuIcon = document.querySelector('#menu-icon')
 let navbar = document.querySelector('.navbar')
 
-menuIcon.onclick = ()=>{
+menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
-    
+
     navLinks.forEach(navLink => {
         navLink.addEventListener('click', () => {
-            navbar.classList.remove('active'); 
+            navbar.classList.remove('active');
             menuIcon.classList.remove('bx-x')
-            
+
         });
     });
 }
@@ -35,22 +35,38 @@ menuIcon.onclick = ()=>{
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
-window.onscroll= ()=>{
-    sections.forEach(sec=>{
+window.onscroll = () => {
+    sections.forEach(sec => {
         let top = window.scrollY;
-        let offset =  sec.offsetTop - 150;
+        let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;;
         let id = sec.getAttribute('id');
 
-        if(top >= offset && top < offset + height){
-            navLinks.forEach(links=>{
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-                
+
             })
         }
     })
     // =====sticky navbar====//
     let header = document.querySelector('header')
-    header.classList.toggle('sticky', window.scrollY>100)
+    header.classList.toggle('sticky', window.scrollY > 100)
+}
+
+
+
+// EMAIL JS
+function sendMail() {
+    let params = {
+        from_name: document.getElementById('fullName').value,
+        email_id : document.getElementById('email_id').value,
+        mobileNumber: document.getElementById('mobileNumber').value,
+        emailSubject: document.getElementById('emailSubject').value,
+        message: document.getElementById('message').value
+    }
+    emailjs.send("service_l2nqzaj", "template_do2d9j2", params).then(function(res){
+        alert("Email sent successfully!")
+    })
 }
